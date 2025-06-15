9212c019223492abdeedfdb84861b6f5d5b9352e
Shabeebkoloth <mohammedshabeebm602@gmail.com>
2025-06-15 19:48:53 +0530

    Updated index.html

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
+
 # My Netflix Clone
 This is my local version of the project. Uploaded to GitHub.
-=======
 # netflix
->>>>>>> 06b6e90fdae7fe6f4c1597e2dc17a6efc8683216
--- src/Constants/constants.js
+++
@@ -1,5 +0,0 @@
-export const API_KEY = "5e94e37fd64110d676523351d92a50b5";
-
-export const imageUrl = "https://image.tmdb.org/t/p/original"
-
-export const baseUrl = "https://api.themoviedb.org/3";
